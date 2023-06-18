6f20b88899e9cc5d57b909d4f1dad258291a7b86
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-18 03:17:12 -0300

    se agrega pagina login y registro

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Administrador from "./components/views/Administrador";
 import DetalleReceta from "./components/views/DetalleReceta";
 import Error404 from "./components/views/Error404";
 import Inicio from "./components/views/Inicio";
+import Login from "./components/views/Login";
+import Registro from "./components/views/Registro";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
     {/* <DetalleReceta></DetalleReceta> */}
     {/* <Error404></Error404> */}
     <Inicio></Inicio>
+    {/* <Login></Login> */}
+    {/* <Registro></Registro> */}
     <Footer></Footer>
     </>
   );
