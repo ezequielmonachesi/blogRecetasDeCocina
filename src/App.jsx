371fd2d67669e941719fa909d9dd2346aab4c84a
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-18 04:23:10 -0300

    creamos las rutas de cada pagina

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,27 @@ import Error404 from "./components/views/Error404";
 import Inicio from "./components/views/Inicio";
 import Login from "./components/views/Login";
 import Registro from "./components/views/Registro";
+import CrearReceta from "./components/views/receta/CrearReceta"
+import EditarReceta from "./components/views/receta/EditarReceta"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
     <Menu></Menu>
-    {/* <Administrador></Administrador>  */}
-    {/* <DetalleReceta></DetalleReceta> */}
-    {/* <Error404></Error404> */}
-    <Inicio></Inicio>
-    {/* <Login></Login> */}
-    {/* <Registro></Registro> */}
-    <Footer></Footer>
-    </>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/registro" element={<Registro></Registro>}></Route>
+        <Route exact path="/login" element={<Login></Login>}></Route>
+        <Route exact path="/login" element={<Login></Login>}></Route>
+        <Route exact path="/detalle" element={<DetalleReceta></DetalleReceta>}></Route>
+        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route exact path="/administrador/crear-receta" element={<CrearReceta></CrearReceta>}></Route>
+        <Route exact path="/administrador/editar-receta" element={<EditarReceta></EditarReceta>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
   );
 }
 
